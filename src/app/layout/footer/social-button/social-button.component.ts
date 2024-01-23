3afbc5424e3d70b8c94f98076cc5532c7d2f63c2
UniBreakfast <unibreakfast1@gmail.com>
2024-01-23 16:13:11 +0200

    add social button component

--- src/app/layout/footer/social-button/social-button.component.ts
+++ src/app/layout/footer/social-button/social-button.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Icon } from 'src/app/core/interfaces/icon';
 
 @Component({
   selector: 'social-button',
   templateUrl: './social-button.component.html',
   styleUrls: ['./social-button.component.scss'],
   standalone: true,
+  imports: [
+    CommonModule,
+  ],
 })
 export class SocialButtonComponent {
-  // public className: string = 'social-button'; 
-  public icon: string = '';
+  @Input() public icon!: Icon;
 }
